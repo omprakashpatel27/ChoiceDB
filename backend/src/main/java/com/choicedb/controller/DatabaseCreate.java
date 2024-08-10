46d81dfdbf9673f95a080f18bde8acabcf24011f
Omprakash Patel <59342793+omprakashpatel27@users.noreply.github.com>
2024-08-10 16:43:05 +0530

    refactor and created create-table api (#2)

---
+++ backend/src/main/java/com/choicedb/controller/DatabaseCreate.java
@@ -0,0 +1,32 @@
+package com.choicedb.controller;
+
+import com.choicedb.service.DatabaseCheck;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/db")
+public class DatabaseCreate {
+
+    DatabaseCheck databaseCheck;
+
+    @PostMapping("/create-database")
+    public ResponseEntity<String> createDatabase(@RequestBody Map<String, Object> JSON){
+        if(databaseCheck.isDatabaseExists(JSON)){
+            // TODO :: service call for mysql/mongodb/redis db to  create db
+            return new ResponseEntity<>("SUCCESS" , HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("Invalid request , db Name not exists!", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-table")
+    public ResponseEntity<String> createTable(@RequestBody Map<String, Object> JSON){
+        if(databaseCheck.isDatabaseExists(JSON) && databaseCheck.isTableExists(JSON)){
+            // TODO :: service call for mysql/mongodb/redis db to create table
+            return new ResponseEntity<>("SUCCESS" , HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("Invalid request , db Name not exists!", HttpStatus.NOT_FOUND);
+    }
+}
